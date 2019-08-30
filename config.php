125422ea8063efbb4955cd688bee1281e0b5c219
modymor8 <50548442+modymor8@users.noreply.github.com>
2019-08-30 17:36:29 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?
+
+$UserAgent = "Mozilla/5.0 (Linux; Android 5.1; 1201) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.101 Mobile Safari/537.36";
+$coinzilla = "%7B%22backed%22%3A%5B%7B%22cap%22%3A4%2C%22lastCall%22%3A1567178171891%7D%5D%7D";
+$gid = "GA1.2.1961197397.1567178142";
+$ga = "GA1.2.1041371443.1567178142";
+$ci_sessions = "jj3785oa6eell4k4kd08mr8vqj1k0kc6";
+$cfduid = "da0b58fb8cb9d1a405bc00d1fc7f7816b1567178132";
